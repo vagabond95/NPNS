e5496949f62fed9590f8689aba8bde2d16c39dfc
JhKim <progjh@gmail.com>
2017-11-20 14:25:39 +0900

    [NPNS-1] first commit

---
+++ app/src/main/java/com/example/npns_demo/client/MqttClient.java
@@ -0,0 +1,8 @@
+package com.example.npns_demo.client;
+
+/**
+ * Created by JHKim on 2017-11-20.
+ */
+
+public class MqttClient {
+}
---
+++ app/src/main/java/com/example/npns_demo/service/MessageService.java
@@ -0,0 +1,26 @@
+package com.example.npns_demo.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MessageService extends Service {
+    public MessageService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+}
