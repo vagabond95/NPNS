405031e89880d85ef5328ec15ce58bd54e165219
JhKim <progjh@gmail.com>
2017-11-23 17:17:27 +0900

    [feature1-1] http helper 클래스, json helper 클래스 작성

---
+++ app/src/main/java/com/example/npns_demo/util/HttpHelper.java
@@ -0,0 +1,57 @@
+package com.example.npns_demo.util;
+
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class HttpHelper {
+
+    private final String TAG = HttpHelper.class.getCanonicalName();
+
+    private MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    private OkHttpClient mClient;
+
+    private String mHostUrl;
+
+    public HttpHelper() {
+        mClient = new OkHttpClient();
+    }
+
+    public void post(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(JSON, json);
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .addHeader("content-type", "application/json; charset=utf-8")
+                .build();
+
+        mClient.newCall(request).enqueue(callbackAfterRequest);
+    }
+
+    private Callback callbackAfterRequest = new Callback() {
+        @Override
+        public void onFailure(Call call, IOException e) {
+            Log.d(TAG, "response fail!");
+
+        }
+
+        @Override
+        public void onResponse(Call call, Response response) throws IOException {
+            Log.d(TAG, "response success!");
+        }
+    };
+}
---
+++ app/src/main/java/com/example/npns_demo/util/JsonHelper.java
@@ -0,0 +1,24 @@
+package com.example.npns_demo.util;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class JsonHelper {
+
+    private Map<String, String> mParams;
+
+    public JsonHelper() {
+        mParams = new HashMap<String, String>();
+    }
+
+    public void putDataToJson(String key, String value) {
+        mParams.put(key, value);
+    }
+
+    public String getJson() {
+        JSONObject parameter = new JSONObject(mParams);
+        return parameter.toString();
+    }
+}
